b02430bb5392f34b76d89a094003fefcce48376d
Jacob West <38504417+jacobrobertwest@users.noreply.github.com>
2026-05-24 09:23:59 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ pygame.font.init()
 
 SOUNDCUBE_MODE = os.getenv("SOUNDCUBE_MODE", "prod")
 
-SOUNDCUBE_VERSION = '1.1.0'
+SOUNDCUBE_VERSION = '1.1.1'
 
 PRIMARY_FONT_FP = 'files/font/Futura.otf'
 GAMECUBE_FONT_FP = 'files/font/GameCube.ttf'
